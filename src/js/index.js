786e2cd8b80fb8b9a8c58af26c4859701dee78a6
evideli <evideli92@gmail.com>
2021-04-07 20:01:23 +0300

    Improved the styling of the navbar, added carousel images, descriptions, buttons and added css styling for them and jquery on click listeners

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,26 @@ import 'bootstrap';
 
 import '../scss/index.scss';
 
+jQuery(function () {
+  console.log("ready!");
+  $(".carousel-inner").css({
+    "background-color": "rgba(black, 0.9)",
+    "zIndex": 1,
+  });
+})
+
 $('#sign-in').on('click', () => {
   alert('Operation not allowed!');
 });
 
-// Your jQuery code
+$('#bootstrap-more-button').on('click', () => {
+  window.open("https://getbootstrap.com/", "_blank");
+});
+
+$('#jquery-more-button').on('click', () => {
+  window.open("https://jquery.com/", "_blank");
+});
+
+$('#webpack-more-button').on('click', () => {
+  window.open("https://webpack.js.org/", "_blank");
+});
