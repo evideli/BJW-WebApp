51deb5b46d4eb909dc381d8af5873a70cfdbc8c6
evideli <evideli92@gmail.com>
2021-04-08 18:43:30 +0300

    Load benefit images with jquery

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,6 @@
 import 'bootstrap';
 import '../css/index.css';
 
-jQuery(function () {
-
-})
-
 $('#sign-in').on('click', () => {
   alert('Operation not allowed!');
 });
@@ -24,3 +20,7 @@ $('#webpack-more-button').on('click', () => {
 $(".form-inline").submit((event) => {
   event.preventDefault();
 });
+
+$("#benefit-img-1").attr("src","https://www.flaticon.com/svg/vstatic/svg/3564/3564796.svg?token=exp=1617897195~hmac=bff1c2fb4fc0ab29174cdde205ca9b34");
+$("#benefit-img-2").attr("src","https://www.flaticon.com/svg/vstatic/svg/3722/3722009.svg?token=exp=1617897324~hmac=dd3eadb4ff38f4e466ef10093c8124bd");
+$("#benefit-img-3").attr("src","https://www.flaticon.com/svg/vstatic/svg/45/45068.svg?token=exp=1617897410~hmac=c20e4f8b2b0aca82670fe5715a4d7e01");
